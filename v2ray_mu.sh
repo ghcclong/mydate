ad5b07f05f028f5f1cfd58bf7ed8d9a4a8e43aca
ghcclong <44447338+ghcclong@users.noreply.github.com>
2019-10-30 21:44:07 +0800

    Add files via upload

--- v2ray_mu.sh
+++ v2ray_mu.sh
@@ -17,13 +17,13 @@ update_kernel(){
     sed -i "0,/enabled=0/s//enabled=1/" /etc/yum.repos.d/epel.repo
     yum remove -y kernel-devel
     rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
-    rpm -Uvh https://www.elrepo.org/elrepo-release-7.0-3.el7.elrepo.noarch.rpm
+    rpm -Uvh https://www.elrepo.org/elrepo-release-7.0-4.el7.elrepo.noarch.rpm
     yum --disablerepo="*" --enablerepo="elrepo-kernel" list available
     yum -y --enablerepo=elrepo-kernel install kernel-ml
     sed -i "s/GRUB_DEFAULT=saved/GRUB_DEFAULT=0/" /etc/default/grub
     grub2-mkconfig -o /boot/grub2/grub.cfg
-    wget https://elrepo.org/linux/kernel/el7/x86_64/RPMS/kernel-ml-devel-4.20.4-1.el7.elrepo.x86_64.rpm
-    rpm -ivh kernel-ml-devel-4.20.4-1.el7.elrepo.x86_64.rpm
+    wget https://elrepo.org/linux/kernel/el7/x86_64/RPMS/kernel-ml-devel-5.3.7-1.el7.elrepo.x86_64.rpm
+    rpm -ivh kernel-ml-devel-5.3.7-1.el7.elrepo.x86_64.rpm
     yum -y --enablerepo=elrepo-kernel install kernel-ml-devel
 	timedatectl set-timezone Asia/Shanghai
     read -p "需要重启实例，是否现在重启 ? [Y/n] :" yn
